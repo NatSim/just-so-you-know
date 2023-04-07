f9b1d65d567c0b1f2c3c2e41ebe6a1e0d40ce06b
NatSim <natsimbig@gmail.com>
2023-04-07 13:45:35 +0100

    🎨format: add spacing to footer content

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,12 +1,32 @@
-import "../components/Footer.css"
+import "../components/Footer.css";
+import { ReactComponent as JustSoYouKnowLogo } from "../images/logo_jsyk.svg";
 
-const Footer =() =>{
-    return( 
+const Footer = () => {
+  return (
     <>
-    <div className="footer">
-     <footer>Footer</footer>   
-    </div>
-    </>)
-}
+      <footer>
+        <div className="footer-container">
+          <div className="footer-content-container">
+            <div className="logo-spacing">
+              <JustSoYouKnowLogo
+                className="footer-logo"
+                alt="just so you know logo"
+              />
+            </div>
+            <div>hello@justsoyouknow.com</div>
+          </div>
+          <div className="footer-content-container">
+            <ul>
+              <li>Quick links</li>
+              <li>About us</li>
+              <li>Contact us</li>
+              <li>Policy</li>
+            </ul>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+};
 
 export default Footer;
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,35 +1,40 @@
 import "../App.css";
 //style sheets
-import "./LandingPage.css"
+import "./LandingPage.css";
 // import ReactDOM from "react-dom/client";
 import { useNavigate } from "react-router-dom";
 //import logo
-import { ReactComponent as JustSoYouKnowLogo } from '../images/logo_jsyk.svg';
-
+import { ReactComponent as JustSoYouKnowLogo } from "../images/logo_jsyk.svg";
 
 const LandingPage = () => {
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   return (
-    <div className="App">         
-     <div className="main-container"> 
-        
-        <h1 className="image-container"><JustSoYouKnowLogo alt="just so you know logo"/></h1>
-       
-         <h2 className="heading">Check My Evening</h2>
-      <div>
-        <input type="text" placeholder="Find a venue"></input>
-      <button className="venue-button radius-right" onClick={()=>navigate("/venue")}>Find a venue</button>
-      </div>
-      <div className="secondary-container">
-        <h2 className="heading">Report Incident</h2>
-        <p className="text">Experienced something unsafe?</p>
-     <button className="report-button" onClick={()=>navigate("/report")} >Report</button>
-      </div>  
-    </div>
-  </div>
+    <div className="App">
+      <div className="main-container">
+        <h1 className="image-container">
+          <JustSoYouKnowLogo alt="just so you know logo" />
+        </h1>
 
+        <h2 className="heading">Check My Evening</h2>
+        <div>
+          <input type="text" placeholder="Find a venue"></input>
+          <button
+            className="venue-button radius-right"
+            onClick={() => navigate("/venue")}
+          >
+            Find a venue
+          </button>
+        </div>
+        <div className="secondary-container">
+          <h2 className="heading">Report Incident</h2>
+          <p className="text">Experienced something unsafe?</p>
+          <button className="report-button" onClick={() => navigate("/report")}>
+            Report
+          </button>
+        </div>
+      </div>
+    </div>
   );
 };
 
 export default LandingPage;
-
